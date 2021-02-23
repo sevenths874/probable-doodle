7eb6f965e7eee8cd52ffd8be9e2e8909108a76f8
sevenths874 <2607520791@qq.com>
2021-02-23 23:11:19 +0800

    匿名对象

---
+++ 类和对象_2_23/类和对象_2_23/源.cpp
@@ -0,0 +1,25 @@
+//匿名对象 
+#include<iostream>
+using namespace std;
+
+class A
+{
+private:
+	int _a;
+	int _b;
+
+public:
+	A()
+	{
+		cout << "A()" << endl;
+	}
+
+};
+
+int main()
+{
+	A a;  //a的生命周期在整个main函数中
+	A();  //匿名对象的生命周期只在这一行
+	system("pause");
+	return 0;
+}
