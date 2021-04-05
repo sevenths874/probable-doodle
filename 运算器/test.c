9815b90ccdbc7df0019cb0fb3493b2faac315861
sevenths874 <2607520791@qq.com>
2021-04-05 16:25:20 +0800

    简易运算器

---
+++ 运算器/test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+void test()
+{
+	int a = 1, b = 2;
+	printf("a=%d b=%d\n");
+	//struct ListNode* l1 = NULL, l2 = NULL;
+}
+
+ 
